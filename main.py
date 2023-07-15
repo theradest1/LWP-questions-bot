3111a7231c7afeca8a2444c51c010e688dd4a5ad
TheRad1 <18326@verona.k12.wi.us>
2023-07-15 18:26:10 -0500

    added gpt (scuffed)

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 from discord.ext import tasks
 import asyncio
+import openai
 
 intents = discord.Intents.all()
 intents.message_content = True
@@ -9,6 +10,46 @@ bot = commands.Bot(command_prefix=">>", intents = intents)
 maxQuestionLength = 17
 questionsParent = "Questions"
 questionsAskID = 1129651287206658070
+
+settingsFile = open("gptSettings.txt")
+settings = settingsFile.readlines()
+gptConversationHistory = []
+
+#try:
+#    gptMaxTokens = int(settings[0])
+#except:
+gptMaxTokens = 50
+
+def resetGPTMem():
+    gptConversationHistory = []
+    
+def setGPTMaxTokens(maxTokens):
+    gptMaxTokens = maxTokens
+
+def getGPTAnswer(question):
+    #get question along with paster conversation
+    #prompt = '\n'.join(gptConversationHistory + [question])
+    #print(prompt)
+    #print(gptMaxTokens)
+    
+    #use api
+    print(question)
+    response = openai.Completion.create(
+        engine='gpt-3.5-turbo',  # Specify the engine for ChatGPT (davinci, curie, or babbage)
+        prompt=question,  # Your prompt or question
+        max_tokens=50,  # Maximum number of tokens in the response
+        n=1,  # Number of responses to generate
+        stop=None,  # Stop the completion on a specific string (optional)
+    )
+
+    #get response
+    answer = response.choices[0].text.strip()
+    
+    #update conversation history
+    #gptConversationHistory.append(question)
+    #gptConversationHistory.append(answer)
+    
+    return answer
 
 @bot.event
 async def on_ready():
@@ -23,9 +64,9 @@ async def ask(ctx, *args):
                 category = await ctx.guild.create_category(questionsParent)
             newChannel = await ctx.guild.create_text_channel(' '.join(args), category = category)
             
-            await ctx.send("Channel created")
+            await ctx.send("Channel created: " + newChannel.mention)
             await newChannel.send(ctx.message.author.mention)
-            await newChannel.send("Here is your channel to ask your question and get responses. Send the command \"/answered\" in this channel to close this once done.")
+            await newChannel.send("Here is your channel to ask your question and get responses. Send the command `>>answered` in this channel to close it.")
         else:
             await ctx.send("Your description is too long, try to shorten it to just a few words (" + str(maxQuestionLength) + " letter maximum).")
     else:
@@ -55,7 +96,23 @@ async def info(ctx):
     embedVar.add_field(name="Close Question:", value="`>>answered`\nrun this command in your question channel", inline=False)
     embedVar.add_field(name="Info:", value="`>>info`\nRun this command anywhere", inline=False)
     await ctx.send(embed=embedVar)
+    
+@bot.command()
+async def gpt(ctx, *args):
+    answer = getGPTAnswer(' '.join(args))
+    await ctx.send(answer)
+    
+@bot.command()
+async def clearGPT(ctx):
+    resetGPTMem()
+    await ctx.send("Chat GPT's memory has been cleared")
+@bot.command()
+async def setGPTTokens(ctx, arg):
+    setGPTMaxTokens(int(arg))
+    await ctx.send("Chat GPT's max tokens has been set to " + arg)
 
+#starting things
 tokenFile = open("token.txt")
 content = tokenFile.readlines()
+openai.api_key = content[3]
 bot.run(content[1])
